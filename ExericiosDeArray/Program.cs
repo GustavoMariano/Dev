55f0b2a49f3ca1532f78ef3420079e8af58ee777
GUSTAVO CESAR MARIANO <gustavocmariano@hotmail.com>
2021-02-16 00:01:26 -0300

    Exercicios de array

---
+++ ExericiosDeArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExericiosDeArray
+{
+     class Program
+     {
+          private static void Main()
+          {
+              String[] nomes = new string[] { "a", "z", "c", "aa", "bc", "cb", };
+
+
+            //Linhas comentadas = ordenar array(strings de A a Z e numéricos do menor para o maior).
+
+            //Array.Sort(nomes);
+            //foreach (string ordenado in nomes)
+            //{
+            //    Console.WriteLine(ordenado);
+            //}
+
+            String[] nomes2 = new string[6];
+
+            nomes.CopyTo(nomes2, 0);
+
+            foreach (string nome in nomes2)
+            {
+                Console.WriteLine(nome);
+            }
+        }
+     }    
+}
---
+++ Maior.Numero/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Maior.Numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[5];
+            int maior =0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            foreach (var numero in numeros)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    if(maior < numeros[i])
+                    {
+                        maior = numero;
+                    }
+                }
+                
+            }
+            Console.WriteLine("\n"+ maior);
+        }
+    }
+}
