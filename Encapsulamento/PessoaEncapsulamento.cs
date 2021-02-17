10ae25a898e8b8ad45dc19819749f384b8937693
GUSTAVO CESAR MARIANO <gustavocmariano@hotmail.com>
2021-02-16 22:20:38 -0300

    Encapsulamento

---
+++ Encapsulamento/PessoaEncapsulamento.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento
+{
+    class PessoaEncapsulamento
+    {
+        private double salario;
+        private string nome;
+
+        public string AlteraNome
+        {
+            get{return this.nome;}
+            set{this.nome = value;}
+        }
+
+        public double AlteraSalario
+        {
+            get
+            {
+                return this.salario;
+            }
+            set
+            {
+                this.salario = value;
+            }
+        }
+    }
+}
---
+++ Encapsulamento/TestaEncapsulamento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Encapsulamento
+{
+    class TestaEncapsulamento
+    {
+        private static void Main(string[] args)
+        {
+            PessoaEncapsulamento pessoa = new PessoaEncapsulamento();
+
+            Console.WriteLine("Digite o Nome: ");
+            pessoa.AlteraNome = Console.ReadLine();
+
+            Console.WriteLine("Digite o salario do "+ pessoa.AlteraNome + ":");
+            pessoa.AlteraSalario = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("O salário do "+ pessoa.AlteraNome +" é: " + pessoa.AlteraSalario);
+        }
+    }
+}
